90275553655c38b0e08532ab1428717dcb2eee8e
Ricardo Martins <rs84martins@gmail.com>
2020-07-23 22:37:26 -0300

    Projeto criado

---
+++ src/application/Program.java
@@ -0,0 +1,30 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Program {
+	public static void main(String[] args) {
+		
+		List<String> list = new ArrayList<>();
+		String path = "C:\\temp\\in_2.txt";
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			String name = br.readLine();
+			while (name != null) {
+				list.add(name);
+				name = br.readLine();
+			}
+			Collections.sort(list); //lista ordenada
+			for (String s : list) {
+				System.out.println(s);
+			}
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+	}
+}
