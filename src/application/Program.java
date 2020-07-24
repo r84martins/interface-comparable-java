8f0e3397cc6c3dd4cd927f265c460488dfe0d2a2
Ricardo Martins <rs84martins@gmail.com>
2020-07-23 23:02:10 -0300

    Interface comparable implementado

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,21 +7,24 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import entities.Employee;
+
 public class Program {
 	public static void main(String[] args) {
 		
-		List<String> list = new ArrayList<>();
-		String path = "C:\\temp\\in_2.txt";
+		List<Employee> list = new ArrayList<>();
+		String path = "C:\\temp\\in_3.txt";
 		
 		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-			String name = br.readLine();
-			while (name != null) {
-				list.add(name);
-				name = br.readLine();
+			String employeeCsv = br.readLine();
+			while (employeeCsv != null) {
+				String[] fields = employeeCsv.split(",");
+				list.add(new Employee(fields[0], Double.parseDouble(fields[1])));
+				employeeCsv = br.readLine();
 			}
 			Collections.sort(list); //lista ordenada
-			for (String s : list) {
-				System.out.println(s);
+			for (Employee emp : list) {
+				System.out.println(emp.getName() + ", " + emp.getSalary());
 			}
 		} catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
